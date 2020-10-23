701bfe2fa09c81f756cee208a1201b55a6b31a3d
Kethan02 <73210328+Kethan02@users.noreply.github.com>
2020-10-22 20:17:50 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '!')
 
+keyWords_List = ['Homework', 'homework', 'hw', 'HW']
+
 @client.event
 async def on_ready():
     print('Bot is ready.')
@@ -17,5 +19,37 @@ async def about(ctx):
 
     await ctx.message.channel.send(embed = myEmbed)
 # use embed to send the newsletter (this is a good idea)
+
+@client.command(name = 'addKeyword', aliases = ['addKW', 'aKW', 'addkeyword'])
+async def about(ctx, *, newKeyWord):
+    keyWords_List.append(newKeyWord)
+
+    if newKeyWord in keyWords_List:
+        await ctx.send('Keyword added successfully')
+    else:
+        await ctx.send('Keyword not added successfully')
+
+@client.command(name = 'myKeywords', aliases = ['myKW', 'mykw', 'mkw'])
+async def about(ctx):
+    for words in keyWords_List:
+        await ctx.send(words)
+
+    await ctx.send('These are your keywords')
+
+@client.event
+async def on_message(message):
+
+    if message.author.id != client.user.id:
+        for key in keyWords_List:
+            if key in message.content:
+                print('Found')
+                myMessageEmbed = discord.Embed(
+                title = "Message Found",
+                description = message.content,
+                colour = discord.colour.blue()
+                )
+
+                await message.channel.send(embed = myMessageEmbed)
+    await client.process_commands(message)
 
 client.run('NzY2NTE5NzU0MTc2MzMxODE2.X4kjJg.tfogUj4ihVX296Gfcjc2hQziCkk')
