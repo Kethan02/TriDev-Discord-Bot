94f8ad5f357ff95ae36ac20ffc7b60ad242b5214
holdena1 <holden.adamec@gmail.com>
2020-10-30 22:16:51 -0700

    Separated get_database and connect_to_mongo functions

--- db.py
+++ db.py
@@ -37,26 +37,39 @@ keyword_document_schema = {
   "keywords": ["hw", "homework", "HW", "Homework"]
 }
 
-def connect_to_db(user, pswd, db_name):
-  """Returns a database object which is used to get data for each guild"""
-  link = f"mongodb+srv://{user}:{pswd}@discorddata.cmilw.mongodb.net/{db_name}?retryWrites=true&w=majority"
+def connect_to_mongo(user, pswd):
+  """
+  Returns a client object that can be used to access all data.
+
+  NOTE: This function is slow. Authenticating takes a few seconds at least.
+  """
+  link = f"mongodb+srv://{user}:{pswd}@discorddata.cmilw.mongodb.net"
   client = MongoClient(link)
-  db = client[db_name]
-  print("Connected successfully to database!")
-  return db
+  print("Connected successfully to mongodb!")
+  return client
 
-def get_collection(db_object, collection_name):
+def get_db(client_obj, db_name):
+  """
+  Returns a database object which is used to get data for each guild
+
+  db_name -- is the name of the desired guild with _'s instead of spaces
+  """
+  return client_obj[db_name]
+
+def get_collection(db_obj, collection_name):
   """Returns a collection contained in a database."""
-  return db_object[collection_name]
+  return db_obj[collection_name]
 
-def get_messages(collection_object, user_name):
+def get_messages(collection_obj, user_name):
   """
-  Returns the messages of a specific guild (collection), with the exception of
+  Returns the messages flagged by the keywords, with the exception of
   messages authored by the given user.
-  """
-  return collection_object.find({ "user": { "$ne": user_name } })
 
-def add_message(collection_object, user_name, message, timestamp):
+  This method should be used when creating the newsletter
+  """
+  return collection_obj.find({ "user": { "$ne": user_name } })
+
+def add_message(collection_obj, user_name, message, timestamp):
   """
   Adds a message document to the database.
   Returns the timestamp of the document if the transaction was successful.
@@ -66,7 +79,7 @@ def add_message(collection_object, user_name, message, timestamp):
     "content": message,
     "timestamp": timestamp
   }
-  x = collection_object.insert_one(document)
+  x = collection_obj.insert_one(document)
   return x.inserted_id.generation_time
 
 def add_keyword(collection_keywords, keyword_category, keywords):
@@ -101,10 +114,12 @@ def get_keywords(collection_keywords):
 
 def main():
   # Connects to database and gets collection (slow)
-  test_server_db = connect_to_db(user, pswd, db_name)
+  client = connect_to_mongo(user, pswd)
+  # Gets a database (fast)
+  test_server_db = get_db(client, db_name)
   # Gets a collection of data (fast)
   keywords_collection = get_collection(test_server_db, "keywords")
-  # Gets and prints out the messages in the collection (very fast)
+  # Gets and prints out the messages in the collection (fast)
   keywords = get_keywords(keywords_collection)
   for w in keywords:
     print(w)
