63fcb146196e02e973fc55131e7308bc0a817a39
yok029 <59902140+yok029@users.noreply.github.com>
2020-10-31 16:54:37 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
+@ -1,5 +1,6 @@
 import discord
 import os
 import asyncio
+import os
 from discord.ext import commands
+# Importing text summerization library. Feel free to make any changes
+# if you think there is a better library for text summarization
+# check the texttest.py file to see how it works.
 
 client = commands.Bot(command_prefix = '!')
+@ -56,3 +57,5 @@ async def on_message(message):
 
+<<<<<<< HEAD
 keyWords_List = ['Homework', 'homework', 'hw', 'HW']
 
 @client.event
@@ -59,3 +66,9 @@ async def on_message(message):
     await client.process_commands(message)
     
 client.run(os.environ['DISCORD_TOKEN'])
+=======
+                await message.channel.send(embed = myMessageEmbed)
+    await client.process_commands(message)
+    
+client.run(os.environ['DISCORD_TOKEN'])
+>>>>>>> database
