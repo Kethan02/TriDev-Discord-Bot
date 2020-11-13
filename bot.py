9215769417711454203a0798d65c0087e5b69e6d
Kethan02 <73210328+Kethan02@users.noreply.github.com>
2020-11-12 18:00:38 -0800

    reverted bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,7 @@
 import discord
 import os
 import asyncio
-import os
 from discord.ext import commands
-from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
-from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
-from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
-
-# Importing text summerization library. Feel free to make any changes
-# if you think there is a better library for text summarization
-# check the texttest.py file to see how it works.
 
 client = commands.Bot(command_prefix = '!')
 
