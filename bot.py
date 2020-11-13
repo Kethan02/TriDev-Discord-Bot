203d46ad28c443e9fb34b271575fa689c1b294b0
Kethan02 <73210328+Kethan02@users.noreply.github.com>
2020-11-12 17:35:35 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-@ -1,5 +1,6 @@
 import discord
 import os
 import asyncio
@@ -13,7 +12,7 @@ from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstra
 # check the texttest.py file to see how it works.
 
 client = commands.Bot(command_prefix = '!')
-@ -56,3 +57,5 @@ async def on_message(message):
+    async def on_message(message):
 
 <<<<<<< HEAD
 keyWords_List = ['Homework', 'homework', 'hw', 'HW']
