04ecd5402c4f950770627fe4928e4987a47036cb
Kethan Bethamcharla <kethanbethamcharla@Kethans-MacBook-Pro.local>
2020-12-01 21:22:34 -0800

    finxing the summary being too long

--- bot.py
+++ bot.py
@@ -122,6 +122,8 @@ async def about(ctx):
 async def about(ctx, *, channel):
     messages = db.get_all_messages(all_messages_collection, ctx.guild.id, channel)
     summary = tfidf_summarizer.run_summarization(messages)
+    while (len(summary) > 6000):
+        summary = tfidf_summarizer.run_summarization(messages)
 
     Embed = discord.Embed(
         title = ("Newsletter from the " + channel + " channel in the " + ctx.guild.name + " server"),
@@ -135,6 +137,8 @@ async def about(ctx, *, channel):
 async def about(ctx, channel, date):
     messages = db.get_all_messages_from_specific_day(all_messages_collection, ctx.guild.id, channel, date)
     summary = tfidf_summarizer.run_summarization(messages)
+    while (len(summary) > 6000):
+        summary = tfidf_summarizer.run_summarization(messages)
 
     Embed = discord.Embed(
         title = ("Newsletter from the " + channel + " channel in the " + ctx.guild.name + " server"),
