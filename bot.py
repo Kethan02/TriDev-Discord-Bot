53e19d0fa9743650013e374e55fb89d4f7182755
Kethan Bethamcharla <kethanbethamcharla@Kethans-MacBook-Pro.local>
2020-12-03 20:39:11 -0800

    fixing splitting paragraph error

--- bot.py
+++ bot.py
@@ -128,8 +128,8 @@ async def about(ctx, *, channel):
         description = "*The summary is sent in multiple paragraphs due to size constrictions when sending it all in one paragraph*"
     )
 
-    llist_summary = ['k']
-    list_summary = splitting_second_half_of_summary(summary)
+    list_summary = ['k']
+    list_summary = list_summary.append(splitting_second_half_of_summary(summary))
     for i in range(len(list_summary)):
         para_number = i+1
         Embed.add_field(name = 'Paragraph'+para_number, value = list_summary[i], inline = False)
@@ -148,7 +148,7 @@ async def about(ctx, channel, date):
     )
 
     list_summary = ['k']
-    list_summary = splitting_second_half_of_summary(summary)
+    list_summary = list_summary.append(splitting_second_half_of_summary(summary))
     for i in range(len(list_summary)):
         para_number = i+1
         Embed.add_field(name = 'Paragraph'+para_number, value = list_summary[i], inline = False)
