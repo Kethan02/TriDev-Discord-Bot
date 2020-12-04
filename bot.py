1d22684ff0182c3310d014ca98a081b2a93eac76
Kethan Bethamcharla <kethanbethamcharla@Kethans-MacBook-Pro.local>
2020-12-03 20:56:22 -0800

    changed problem with append method for list)

--- bot.py
+++ bot.py
@@ -129,7 +129,7 @@ async def about(ctx, *, channel):
     )
 
     list_summary = ['k']
-    list_summary = list_summary.append(splitting_second_half_of_summary(summary))
+    list_summary.append(splitting_second_half_of_summary(summary))
     for i in range(len(list_summary)):
         para_number = i+1
         Embed.add_field(name = 'Paragraph'+para_number, value = list_summary[i], inline = False)
@@ -148,7 +148,7 @@ async def about(ctx, channel, date):
     )
 
     list_summary = ['k']
-    list_summary = list_summary.append(splitting_second_half_of_summary(summary))
+    list_summary.append(splitting_second_half_of_summary(summary))
     for i in range(len(list_summary)):
         para_number = i+1
         Embed.add_field(name = 'Paragraph'+para_number, value = list_summary[i], inline = False)
