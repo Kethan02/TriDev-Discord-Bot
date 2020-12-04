4b7fa34ada16faa17c956837cdfc643dd1a614d9
Kethan Bethamcharla <kethanbethamcharla@Kethans-MacBook-Pro.local>
2020-12-03 21:23:27 -0800

    more test

--- bot.py
+++ bot.py
@@ -129,7 +129,7 @@ async def about(ctx, *, channel):
     )
 
     list_summary = ['k']
-    list_summary = splitting_second_half_of_summary(summary, list_summary)
+    await ctx.author.send(splitting_second_half_of_summary(summary))
     for i in range(len(list_summary)):
         para_number = i+1
         Embed.add_field(name = 'Paragraph'+str(para_number), value = list_summary[i], inline = False)
