8462b8531f91b5af4469c4563ac8b8878b612585
Kethan Bethamcharla <kethanbethamcharla@Kethans-MacBook-Pro.local>
2020-12-03 21:01:30 -0800

    concantenation error

--- bot.py
+++ bot.py
@@ -132,7 +132,7 @@ async def about(ctx, *, channel):
     list_summary.append(splitting_second_half_of_summary(summary))
     for i in range(len(list_summary)):
         para_number = i+1
-        Embed.add_field(name = 'Paragraph'+para_number, value = list_summary[i], inline = False)
+        Embed.add_field(name = 'Paragraph'+str(para_number), value = list_summary[i], inline = False)
 
     await ctx.author.send(embed = Embed)
 
@@ -151,7 +151,7 @@ async def about(ctx, channel, date):
     list_summary.append(splitting_second_half_of_summary(summary))
     for i in range(len(list_summary)):
         para_number = i+1
-        Embed.add_field(name = 'Paragraph'+para_number, value = list_summary[i], inline = False)
+        Embed.add_field(name = 'Paragraph'+str(para_number), value = list_summary[i], inline = False)
 
     await ctx.author.send(embed = Embed)
 
